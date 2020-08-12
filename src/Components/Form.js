af18d0ae73700847bc5b387009b7a0a1e5e3866b
angelynaliem <angelynaliem@gmail.com>
2020-08-11 22:18:01 -0400

    Created form schema

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -7,6 +7,55 @@ import * as yup from 'yup';
 
 const Form = () => {
 
+    //Creating form schema using Yup
+    const formSchema = yup.object().shape({
+
+        name: yup.string().required("Full name is required"),
+        email: yup.string().email("Must be a valid email").required("Email address is required"),
+        password: yup.string().min(6, "Passwords must be at least 6 characters long").required("Password is required"),
+        role: yup.string().oneOf(["Full Stack Web Developer", "Data Scientist", "Web Designer", "Mobile Developer"], "Please pick a role"),
+        terms: yup.boolean().oneOf([true], "Please agree to the Terms and Conditions")
+
+    })
+
+    //Creating validations using Yup
+    const validateChange = (e) => {
+
+        yup
+        .reach()
+
+
+
+    }
+
+
+
+
+//      // inline validation, validating one key/value pair at a time
+//   const validateChange = (e) => {
+//     // get the rules out of schema with reach at key "e.target.name" --> "formSchema[e.target.name]"
+
+//     yup
+//       .reach(formSchema, e.target.name)
+//       .validate(e.target.name === "terms" ? e.target.checked : e.target.value) // compare with real value. does it break the rule?
+//       .then((valid) => {
+//         // if valid param is true, then erase any errors in error state at that key/value in errors
+//         setErrors({
+//           ...errors,
+//           [e.target.name]: ""
+//         });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+
+//         // if failing validation, set error in state
+//         setErrors({
+//           ...errors,
+//           [e.target.name]: err.errors[0]
+//         });
+//       });
+//   };
+
     return (
 
         <form >
@@ -37,6 +86,20 @@ const Form = () => {
                     placeholder = "Create your password"
                     />
 
+            </label>
+
+            <label htmlFor = "role">
+                What is your role?
+                <select
+                    id = "role"
+                    name = "role">
+                        <option> Pick a role </option>
+                        <option value = "Full Stack Web Developer">Full Stack Web Developer</option>
+                        <option value = "Data Scientist">Data Scientist</option>
+                        <option value = "Web Designer">Web Designer</option>
+                        <option value = "Mobile Developer">Mobile Developer</option>
+
+                    </select>
             </label>
 
             <label htmlFor = "terms">
