f29571fb0b11c4ff96f6854c39814aa8c302b406
angelynaliem <angelynaliem@gmail.com>
2020-08-11 21:57:29 -0400

    Added Form markup

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,8 +8,54 @@ import * as yup from 'yup';
 const Form = () => {
 
     return (
-        <h1>Form Goes Here!</h1>
-    )
+
+        <form >
+
+            <label htmlFor="name">
+                <input
+                    id = "name"
+                    type = "text"
+                    name = "name"
+                    placeholder = "Your full name goes here"
+                    />
+            </label>
+
+            <label htmlFor="email">
+                <input 
+                    id = "email"
+                    type = "email"
+                    name = "email"
+                    placeholder = "Your email address goes here"
+                    />
+            </label>
+
+            <label htmlFor = "password">
+                <input
+                    id = "password"
+                    type = "password"
+                    name = "password"
+                    placeholder = "Create your password"
+                    />
+
+            </label>
+
+            <label htmlFor = "terms">
+                <input
+                    id = "terms"
+                    type = "checkbox"
+                    name = "terms"
+                    />
+                     I agree to the Terms and Conditions.
+
+            </label>
+
+            <button type = "submit">
+                Submit
+            </button>
+
+        </form>
+      
+    );
 }
 
 
