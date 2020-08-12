957c8a29588109b42812e1679b4bd378edf3d61b
angelynaliem <angelynaliem@gmail.com>
2020-08-11 21:39:59 -0400

    Rendered Form, import Axios and Yup

---
+++ src/Components/Form.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import * as yup from 'yup';
+
+
+
+
+const Form = () => {
+
+    return (
+        <h1>Form Goes Here!</h1>
+    )
+}
+
+
+export default Form;
