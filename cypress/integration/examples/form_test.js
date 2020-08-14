dc00e6370cbf1d9f40305ed8d04eb729943e6ff3
angelynaliem <angelynaliem@gmail.com>
2020-08-13 21:59:41 -0400

    Added tests in form_test

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -9,23 +9,23 @@ describe("Testing form inputs", () => {
     // you can use "specify" instead of it
     it("adding text to inputs and submits the form", () => {
       // make sure you add the data-cy attribute to your actual code that you're testing!
-      cy.get("[data-cy=name]").type("Emily").should("have.value", "Emily");
+      cy.get("[data-cy=name]").type("Angelyn Liem").should("have.value", "Angelyn Liem");
       // create test for email
       cy.get("[data-cy=email]")
-        .type("test@gmail.com")
-        .should("have.value", "test@gmail.com");
+        .type("angelynaliem@gmail.com")
+        .should("have.value", "angelynaliem@gmail.com");
   
-      cy.get("[data-cy=motivation]")
-        .type("I want to help")
-        .should("have.value", "I want to help");
+      cy.get("[data-cy=password]")
+        .type("123456")
+        .should("have.value", "123456");
   
-      cy.get("[data-cy=positions]")
-        .select("Tabling") // select by option "value" str in Form.js
-        .should("have.value", "Tabling");
+      cy.get("[data-cy=role]")
+        .select("Full Stack Web Developer") // select by option "value" str in Form.js
+        .should("have.value", "Full Stack Web Developer");
   
-      cy.screenshot("my-name-of-image"); // takes a screenshot at this point in time where the position has been selected by the checkbox has not been checked
       cy.get("[data-cy=terms]").check().should("be.checked"); // checks a type="checkbox" input
   
       cy.get("[data-cy=submit]").click();
+      cy.screenshot("my-name-of-image"); // takes a screenshot at this point in time where the position has been selected by the checkbox has not been checked
     });
   });
